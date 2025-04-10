168171885a8d3b0192973a28e708f5719e9a0ac5
ramazziniade <ramazziniade@gmail.com>
2025-04-09 23:21:16 -0300

    Start Project

---
+++ src/IMediator.cs
@@ -0,0 +1,11 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoHorse.Madiator;
+
+public interface IMediator
+{
+    Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+    Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default)
+        where TNotification : INotification;
+}
---
+++ src/INotification.cs
@@ -0,0 +1,3 @@
+namespace GoHorse.Madiator;
+
+public interface INotification { }
